d8d2bf0e84a5d08debcfe5c450f711d20f7430a0
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-06-07 20:55:44 -0700

    Update mcafe-issue.py

--- src/server/opinion/scripts/mcafe-issue.py
+++ src/server/opinion/scripts/mcafe-issue.py
@@ -69,7 +69,7 @@ for s in statements:
         value = np.median(s_rating_list)
         skip=np.array([s_skip])
         hist=np.concatenate((hist,skip), axis=1)
-        
+        print hist
         for i in range(len(hist)-1):
             row=[i,hist[i]]
             writer.writerow(row)
