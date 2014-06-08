7bbf3e21b67a9dee3f3aa397b6e5f0043bbd2046
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-06-07 20:26:17 -0700

    calculate median based on the most recent rating

--- src/server/opinion/scripts/recalculate_median_grades.py
+++ src/server/opinion/scripts/recalculate_median_grades.py
@@ -5,29 +5,50 @@ import numpy as np
 
 active_users = list(User.objects.filter(is_active=True))
 skip_begin_date=datetime.datetime(2014,1,9,0,0,0,0)
+statements = OpinionSpaceStatement.objects.all().order_by('id')
 
-for s in OpinionSpaceStatement.objects.all():
-
-    s_rating=UserRating.objects.filter(opinion_space_statement=s,is_current=True,user__in = active_users)
+for s in statements:
     s_rating_list=[]
-    for rating in s_rating:
-        if rating.created>=skip_begin_date:
-           visitor=Visitor.objects.filter(user=rating.user)  #get the visitor of the user
-           if len(visitor)>0:
-              s_log_skip=LogUserEvents.objects.filter(is_visitor=True, logger_id=visitor[0].id,log_type=11,details__contains='skip').filter(details__contains=str(s.id)).order_by('-created') #get issue skip log
-              s_log_rating=LogUserEvents.objects.filter(is_visitor=True, logger_id=visitor[0].id,log_type=11).exclude(details__contains='skip').filter(details__startswith='slider_set '+str(s.id)).order_by('-created')
-              if len(s_log_skip)==0: #no skip
-                  if len(s_log_rating)>0:
-                     s_rating_list.append(rating.rating)
-              else:
-                  if len(s_log_rating)>0:  #click skip, not move slider s => skip
-                     if s_log_skip[0].created<=s_log_rating[0].created: #final decision is skip
-                        s_rating_list.append(rating.rating)
-           else: 
-              s_rating_list.append(rating.rating)
-        else:
-           s_rating_list.append(rating.rating)
-    
+    s_skip=0
+    for user in active_users:
+        user_s_rating=UserRating.objects.filter(opinion_space_statement=s,user=user).order_by('-created')
+        if len(user_s_rating)==1: #only rate 1 time, get visitor info
+            visitor=Visitor.objects.filter(user=user_s_rating[0].user)
+            if len(visitor)>0:
+                s_log_skip=LogUserEvents.objects.filter(is_visitor=True, logger_id=visitor[0].id,log_type=11,details__contains='skip').filter(details__contains='slider_set '+str(s.id)).order_by('-created')
+                s_log_rating=LogUserEvents.objects.filter(is_visitor=True, logger_id=visitor[0].id,log_type=11).exclude(details__contains='skip').filter(details__contains='slider_set '+str(s.id)).order_by('-created')
+                if len(s_log_skip)==0: #no skip
+                    if len(s_log_rating)>0:
+                        s_rating_list.append(user_s_rating[0].rating)
+                    else: #not click on skip, not move slider s, => skip
+                        s_skip=s_skip+1
+                else:
+                    if len(s_log_rating)==0:  #click skip, not move slider s => skip
+                        s_skip=s_skip+1
+                    else:
+                        if s_log_skip[0].created>s_log_rating[0].created: #final decision is skip
+                            s_skip=s_skip+1
+                        else:
+                            s_rating_list.append(user_s_rating[0].rating)
+            else:
+                s_rating_list.append(user_s_rating[0].rating)
+        if len(user_s_rating)>1: #rate more than 1 time, get user log info
+            s_log_skip=LogUserEvents.objects.filter(is_visitor=False, logger_id=user_s_rating[0].user.id,log_type=11,details__contains='skip').filter(details__contains=str(len(user_s_rating))+' visit ').filter(details__contains=str(s.id)).order_by('-created')
+            s_log_rating=LogUserEvents.objects.filter(is_visitor=False, logger_id=user_s_rating[0].user.id,log_type=11).exclude(details__contains='skip').filter(details__contains=str(len(user_s_rating))+' visit ').filter(details__contains='slider_set '+str(s.id)).order_by('-created')
+            if len(s_log_skip)==0: #no skip
+                if len(s_log_rating)>0:
+                    s_rating_list.append(user_s_rating[0].rating)
+                else: #not click on skip, not move slider s, => skip
+                    s_skip=s_skip+1
+            else:
+                if len(s_log_rating)==0:  #click skip, not move slider s => skip
+                    s_skip=s_skip+1
+                else:
+                    if s_log_skip[0].created>s_log_rating[0].created: #final decision is skip
+                        s_skip=s_skip+1
+                    else:
+                        s_rating_list.append(user_s_rating[0].rating)
+
     cache = StatementMedians.objects.filter(statement = s)
     value = np.median(s_rating_list)
     if cache.count() == 0:
