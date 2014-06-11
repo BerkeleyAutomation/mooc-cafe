5912b31fc3b780d95788750a3923479db160d1b7
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-06-10 23:02:28 -0700

    Update mcafe-send-email.py

--- src/server/opinion/scripts/mcafe-send-email.py
+++ src/server/opinion/scripts/mcafe-send-email.py
@@ -11,21 +11,20 @@ today_date=datetime.datetime.today()-datetime.timedelta(days=1)
 user_today=User.objects.filter(date_joined__gte=today_date)
 
 
-#for user in user_today:
-entrycode=EntryCode.objects.filter(username='hunallen@gmail.com')
+for user in user_today:
+    entrycode=EntryCode.objects.filter(username=user.username)
     
-if len(entrycode)>0:
-    subject = "Your unique link to the CS169.2x MCAFE"
-    email_list = ['hunallen@gmail.com']
-    message = render_to_string('registration/mcafe-confirmation.txt',
-                                  {'entrycode': entrycode[0].code,
-                                    })
-    email = EmailMessage(subject, message, Settings.objects.string('DEFAULT_FROM_EMAIL'),
-                            email_list,
-                            headers = {'Reply-To': 'cafe.mooc@gmail.com'})
+    if len(entrycode)>0:
+        subject = "Your unique link to the CS169.2x MCAFE"
+        email_list = [user.username]
+        message = render_to_string('registration/mcafe-confirmation.txt',
+                                   {'entrycode': entrycode[0].code,
+                                   })
+        email = EmailMessage(subject, message, Settings.objects.string('DEFAULT_FROM_EMAIL'),
+                    email_list, headers = {'Reply-To': 'cafe.mooc@gmail.com'})
     try:
-        email.send()
-        print email
+        #email.send()
+        print user.username
         time.sleep(0.3)
     except:
         pass
