1072e3a90043071dd53702d95653ccd16f532d64
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-06-23 17:05:58 -0700

    Update mcafe-issue.py

--- src/server/opinion/scripts/mcafe-issue.py
+++ src/server/opinion/scripts/mcafe-issue.py
@@ -47,12 +47,13 @@ for s in statements:
             else:
                 s_rating_list.append(user_s_rating[0].rating)
         if len(user_s_rating)>1: #rate more than 1 time, get user log info
-            s_log_skip=LogUserEvents.objects.filter(is_visitor=False, logger_id=user_s_rating[0].user.id,log_type=11,details__contains='skip').filter(details__contains='slider_set '+str(s.id)).order_by('-created')
-            s_log_rating=LogUserEvents.objects.filter(is_visitor=False, logger_id=user_s_rating[0].user.id,log_type=11).exclude(details__contains='skip').exclude(details__contains='grade').filter(details__contains='slider_set '+str(s.id)).order_by('-created')
+            s_log_skip=LogUserEvents.objects.filter(is_visitor=False, logger_id=user_s_rating[0].user.id,log_type=11,details__contains='skip').filter(details__contains=str(len(user_s_rating))+' visit ').filter(details__contains=str(s.id)).order_by('-created')
+            s_log_rating=LogUserEvents.objects.filter(is_visitor=False, logger_id=user_s_rating[0].user.id,log_type=11).exclude(details__contains='skip').filter(details__contains=str(len(user_s_rating))+' visit ').filter(details__contains='slider_set '+str(s.id)).order_by('-created')
             if len(s_log_skip)==0: #no skip
                 if len(s_log_rating)>0:
-                    rating=s_log_rating[0].details.split()
-                    s_rating_list.append(rating[len(rating)-1])
+                    s_rating_list.append(user_s_rating[0].rating)
+                else: #not click on skip, not move slider s, => skip
+                    s_skip=s_skip+1
             else:
                 if len(s_log_rating)==0:  #click skip, not move slider s => skip
                     s_skip=s_skip+1
@@ -60,8 +61,7 @@ for s in statements:
                     if s_log_skip[0].created>s_log_rating[0].created: #final decision is skip
                         s_skip=s_skip+1
                     else:
-                        rating=s_log_rating[0].details.split()
-                        s_rating_list.append(rating[len(rating)-1])
+                        s_rating_list.append(user_s_rating[0].rating)
 
     if len(s_rating_list)>0:
         ofile  = open(settings.MEDIA_ROOT + "/mobile/stats_data/"+"issue"+str(s.id)+"_r.csv", "wb")
