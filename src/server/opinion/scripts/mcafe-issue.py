a9ba73329c576be42e34a45f556405bdcc5d3cfd
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-06-23 17:43:50 -0700

    Update mcafe-issue.py

--- src/server/opinion/scripts/mcafe-issue.py
+++ src/server/opinion/scripts/mcafe-issue.py
@@ -25,15 +25,16 @@ for s in statements:
     s_rating_list=[]
     s_skip=0
     for user in active_users:
-        user_s_rating=UserRating.objects.filter(opinion_space_statement=s,user=user).order_by('-created')
+        user_s_rating=UserRating.objects.filter(opinion_space_statement=s,user=user).order_by('created')
         if len(user_s_rating)==1: #only rate 1 time, get visitor info
-            visitor=Visitor.objects.filter(user=user_s_rating[0].user)
+            visitor=Visitor.objects.filter(user=user)
             if len(visitor)>0:
                 s_log_skip=LogUserEvents.objects.filter(is_visitor=True, logger_id=visitor[0].id,log_type=11,details__contains='skip').filter(details__contains='slider_set '+str(s.id)).order_by('-created')
                 s_log_rating=LogUserEvents.objects.filter(is_visitor=True, logger_id=visitor[0].id,log_type=11).exclude(details__contains='skip').filter(details__contains='slider_set '+str(s.id)).order_by('-created')
                 if len(s_log_skip)==0: #no skip
                     if len(s_log_rating)>0:
-                        s_rating_list.append(user_s_rating[0].rating)
+                        rating=s_log_rating[0].details.split()
+                        s_rating_list.append(float(rating[len(rating)-1]))
                     else: #not click on skip, not move slider s, => skip
                         s_skip=s_skip+1
                 else:
@@ -43,12 +44,13 @@ for s in statements:
                         if s_log_skip[0].created>s_log_rating[0].created: #final decision is skip
                             s_skip=s_skip+1
                         else:
-                            s_rating_list.append(user_s_rating[0].rating)
+                            rating=s_log_rating[0].details.split()
+                            s_rating_list.append(float(rating[len(rating)-1]))
             else:
                 s_rating_list.append(user_s_rating[0].rating)
         if len(user_s_rating)>1: #rate more than 1 time, get user log info
-            s_log_skip=LogUserEvents.objects.filter(is_visitor=False, logger_id=user_s_rating[0].user.id,log_type=11,details__contains='skip').filter(details__contains='slider_set '+str(s.id)).order_by('-created')
-            s_log_rating=LogUserEvents.objects.filter(is_visitor=False, logger_id=user_s_rating[0].user.id,log_type=11).exclude(details__contains='skip').exclude(details__contains='grade').filter(details__contains='slider_set '+str(s.id)).order_by('-created')
+            s_log_skip=LogUserEvents.objects.filter(is_visitor=False, logger_id=user.id,log_type=11,details__contains='skip').filter(details__contains='slider_set '+str(s.id)).order_by('-created')
+            s_log_rating=LogUserEvents.objects.filter(is_visitor=False, logger_id=user.id,log_type=11).exclude(details__contains='skip').exclude(details__contains='grade').filter(details__contains='slider_set '+str(s.id)).order_by('-created')
             if len(s_log_skip)==0: #no skip
                 if len(s_log_rating)>0:
                     rating=s_log_rating[0].details.split()
@@ -62,18 +64,18 @@ for s in statements:
                     else:
                         rating=s_log_rating[0].details.split()
                         s_rating_list.append(float(rating[len(rating)-1]))
-
+    
     if len(s_rating_list)>0:
         ofile  = open(settings.MEDIA_ROOT + "/mobile/stats_data/"+"issue"+str(s.id)+"_r.csv", "wb")
         writer=csv.writer(ofile,delimiter=',')
         title=['score','total']
         writer.writerow(title)
-
+        
         hist,bin_edges = np.histogram(s_rating_list,bins=11,range=(0,1),normed=False)
         value = np.median(s_rating_list)
         skip=np.array([s_skip])
         hist=np.concatenate((hist,skip), axis=1)
-
+        
         for i in range(len(hist)-1):
             row=[i,hist[i]]
             writer.writerow(row)
