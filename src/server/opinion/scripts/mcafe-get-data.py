98b66d7659c450461a09fc61fd9ba492bd3ec77b
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-07-09 21:37:42 -0700

    Update mcafe-get-data.py

--- src/server/opinion/scripts/mcafe-get-data.py
+++ src/server/opinion/scripts/mcafe-get-data.py
@@ -402,7 +402,7 @@ for i in range(len(user)):
     for j in range(len(ideas)):
         user_comment_map.setdefault(i,[]).append(ideas[j].id)
 
-scipy.io.savemat('mcafe_data_1.mat', dict(baseline_issues_1st=baseline_issues_1st,baseline_issues_2nd=baseline_issues_2nd,comment_ratings=comment_ratings,participation=participation,userid=userid,countrymap=countrymap,regionmap=regionmap,gendermap=gendermap,agemap=agemap,visitTimes=visitTimes,college=college,grade_week2=grade_week2,number_rating_ideas=number_rating_ideas,number_submit_ideas=number_submit_ideas,submit_week1=submit_week1,rate_week1=rate_week1,join_week=join_week,user_comment_map=user_comment_map))
+scipy.io.savemat('mcafe_data_1.mat', dict(comment_ratings=comment_ratings,participation=participation,userid=userid,countrymap=countrymap,regionmap=regionmap,gendermap=gendermap,agemap=agemap,visitTimes=visitTimes,college=college,number_rating_ideas=number_rating_ideas,number_submit_ideas=number_submit_ideas,user_comment_map=user_comment_map))
 
 
 
