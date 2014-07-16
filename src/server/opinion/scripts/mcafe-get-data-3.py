6b31d74c843072f33ae4dd7556e40884e0c8d8e5
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-07-16 16:34:24 -0700

    Update mcafe-get-data-3.py

--- src/server/opinion/scripts/mcafe-get-data-3.py
+++ src/server/opinion/scripts/mcafe-get-data-3.py
@@ -28,6 +28,7 @@ rate_6th_date=datetime.datetime(2014,7,17,10,0,0)
 rate_7th_date=datetime.datetime(2014,7,24,10,0,0)
 
 visitors_1=Visitor.objects.filter(created__gte=rate_1st_date,created__lt=rate_2nd_date)
+grade_week1_v=np.ones(len(visitors_1))
 #get 1st week visitor grades(not registed as user)
 baseline_issues_1st_v=-1*np.ones((len(visitors_1),5))
 for s in statements:
@@ -40,6 +41,7 @@ for s in statements:
                 baseline_issues_1st_v[i,s.id-1]=float(rating[len(rating)-1])
             else: #not click on skip, not move slider s, => skip
                 baseline_issues_1st_v[i,s.id-1]=-1
+                grade_week1_v[i]=0
         else:
             if len(s_log_rating)==0:  #click skip, not move slider s => skip
                 baseline_issues_1st_v[i,s.id-1]=-1
@@ -52,6 +54,7 @@ for s in statements:
 
 
 visitors_2=Visitor.objects.filter(created__gte=rate_2nd_date,created__lt=rate_3rd_date)
+grade_week2_v=np.ones(len(visitors_2))
 #get 2nd week visitor grades(not registed as user)
 baseline_issues_2nd_v=-1*np.ones((len(visitors_2),5))
 for s in statements:
@@ -64,6 +67,7 @@ for s in statements:
                 baseline_issues_2nd_v[i,s.id-1]=float(rating[len(rating)-1])
             else: #not click on skip, not move slider s, => skip
                 baseline_issues_2nd_v[i,s.id-1]=-1
+                grade_week2_v[i]=0
         else:
             if len(s_log_rating)==0:  #click skip, not move slider s => skip
                 baseline_issues_2nd_v[i,s.id-1]=-1
@@ -76,6 +80,7 @@ for s in statements:
 
 
 visitors_3=Visitor.objects.filter(created__gte=rate_3rd_date,created__lt=rate_4th_date)
+grade_week3_v=np.ones(len(visitors_3))
 #get 3rd week visitor grades(not registed as user)
 baseline_issues_3rd_v=-1*np.ones((len(visitors_3),5))
 for s in statements:
@@ -88,6 +93,7 @@ for s in statements:
                 baseline_issues_3rd_v[i,s.id-1]=float(rating[len(rating)-1])
             else: #not click on skip, not move slider s, => skip
                 baseline_issues_2nd_v[i,s.id-1]=-1
+                grade_week3_v[i]=0
         else:
             if len(s_log_rating)==0:  #click skip, not move slider s => skip
                 baseline_issues_3rd_v[i,s.id-1]=-1
@@ -99,7 +105,8 @@ for s in statements:
                     baseline_issues_3rd_v[i,s.id-1]=float(rating[len(rating)-1])
 
 visitors_4=Visitor.objects.filter(created__gte=rate_4th_date,created__lt=rate_5th_date)
-#get 3rd week visitor grades(not registed as user)
+#get 4th week visitor grades(not registed as user)
+grade_week4_v=np.ones(len(visitors_4))
 baseline_issues_4th_v=-1*np.ones((len(visitors_4),5))
 for s in statements:
     for i in range(len(visitors_4)):
@@ -111,6 +118,7 @@ for s in statements:
                 baseline_issues_4th_v[i,s.id-1]=float(rating[len(rating)-1])
             else: #not click on skip, not move slider s, => skip
                 baseline_issues_4th_v[i,s.id-1]=-1
+                grade_week4_v[i]=0
         else:
             if len(s_log_rating)==0:  #click skip, not move slider s => skip
                 baseline_issues_4th_v[i,s.id-1]=-1
@@ -123,5 +131,5 @@ for s in statements:
 
 visitor_number=[len(visitors_1),len(visitors_2),len(visitors_3),len(visitors_4)]
 
-scipy.io.savemat('mcafe_data_3.mat', dict(baseline_issues_1st_v=baseline_issues_1st_v,baseline_issues_2nd_v=baseline_issues_2nd_v,baseline_issues_3rd_v=baseline_issues_3rd_v,baseline_issues_4th_v=baseline_issues_4th_v,visitor_number=visitor_number))
+scipy.io.savemat('mcafe_data_3.mat', dict(baseline_issues_1st_v=baseline_issues_1st_v,baseline_issues_2nd_v=baseline_issues_2nd_v,baseline_issues_3rd_v=baseline_issues_3rd_v,baseline_issues_4th_v=baseline_issues_4th_v,visitor_number=visitor_number,grade_week1_v=grade_week1_v,grade_week2_v=grade_week2_v,grade_week3_v=grade_week3_v,grade_week4_v=grade_week4_v))
 
