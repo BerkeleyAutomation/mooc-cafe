21f902dae73e8dd68a3a97bca2968098cba3a104
Ching-Chang (Allen) Huang <hunallen@users.noreply.github.com>
2014-07-28 13:34:39 -0700

    Update mcafe-get-data-2.py

--- src/server/opinion/scripts/mcafe-get-data-2.py
+++ src/server/opinion/scripts/mcafe-get-data-2.py
@@ -25,7 +25,7 @@ rate_4th_date=datetime.datetime(2014,7,3,10,0,0)
 rate_5th_date=datetime.datetime(2014,7,10,10,0,0)
 rate_6th_date=datetime.datetime(2014,7,17,10,0,0)
 rate_7th_date=datetime.datetime(2014,7,24,10,0,0)
-
+rate_end_date=datetime.datetime(2014,7,31,0,0,0)
 #1st week and 1st time rating baseline issues user's grade
 
 baseline_issues_1st=-1*np.ones((len(user),5))
@@ -343,7 +343,7 @@ for s in statements:
                         baseline_issues_5th[i,s.id-1]=float(rating[len(rating)-1])
 
 
-#appear 4th week
+#appear 5th week
 appear_week5=np.zeros(len(user))
 for i in range(len(user)):
     if user[i].date_joined>=rate_5th_date and user[i].date_joined<rate_6th_date:
@@ -353,7 +353,7 @@ for i in range(len(user)):
         if len(s_log)>0:
             appear_week5[i]=1
 
-#grade 4th week
+#grade 5th week
 grade_week5=np.zeros(len(user))
 for i in range(len(user)):
     if user[i].date_joined>=rate_5th_date and user[i].date_joined<rate_6th_date:
@@ -364,17 +364,85 @@ for i in range(len(user)):
             grade_week5[i]=1
 
 
-#number of submit ideas in the 4th week
+#number of submit ideas in the 5th week
 submit_week5=np.zeros(len(user))
 for i in range(len(user)):
     comments=DiscussionComment.objects.filter(user=user[i],created__gte=rate_5th_date,created__lt=rate_6th_date)
     submit_week5[i]=len(comments)
 
-#number of rated ideas in the 4th week
+#number of rated ideas in the 5th week
 rate_week5=np.zeros(len(user))
 for i in range(len(user)):
     ratings=CommentAgreement.objects.filter(rater=user[i],created__gte=rate_5th_date,created__lt=rate_6th_date)
     rate_week5[i]=len(ratings)
+
+#appear 6th week
+appear_week6=np.zeros(len(user))
+for i in range(len(user)):
+    if user[i].date_joined>=rate_6th_date and user[i].date_joined<rate_7th_date:
+        appear_week6[i]=1
+    else:
+        s_log=LogUserEvents.objects.filter(is_visitor=False, logger_id=user[i].id).filter(created__gte=rate_6th_date,created__lt=rate_7th_date)
+        if len(s_log)>0:
+            appear_week6[i]=1
+
+#grade 6th week
+grade_week6=np.zeros(len(user))
+for i in range(len(user)):
+    if user[i].date_joined>=rate_6th_date and user[i].date_joined<rate_7th_date:
+        grade_week6[i]=1
+    else:
+        s_log=LogUserEvents.objects.filter(is_visitor=False, logger_id=user[i].id,log_type=11).filter(created__gte=rate_6th_date,created__lt=rate_7th_date)
+        if len(s_log)>0:
+            grade_week6[i]=1
+
+
+#number of submit ideas in the 6th week
+submit_week6=np.zeros(len(user))
+for i in range(len(user)):
+    comments=DiscussionComment.objects.filter(user=user[i],created__gte=rate_6th_date,created__lt=rate_7th_date)
+    submit_week6[i]=len(comments)
+
+#number of rated ideas in the 6th week
+rate_week6=np.zeros(len(user))
+for i in range(len(user)):
+    ratings=CommentAgreement.objects.filter(rater=user[i],created__gte=rate_6th_date,created__lt=rate_7th_date)
+    rate_week6[i]=len(ratings)
+
+#appear 7th week
+appear_week7=np.zeros(len(user))
+for i in range(len(user)):
+    if user[i].date_joined>=rate_7th_date and user[i].date_joined<rate_end_date:
+        appear_week7[i]=1
+    else:
+        s_log=LogUserEvents.objects.filter(is_visitor=False, logger_id=user[i].id).filter(created__gte=rate_7th_date,created__lt=rate_end_date)
+        if len(s_log)>0:
+            appear_week7[i]=1
+
+#grade 7th week
+grade_week7=np.zeros(len(user))
+for i in range(len(user)):
+    if user[i].date_joined>=rate_7th_date and user[i].date_joined<rate_end_date:
+        grade_week7[i]=1
+    else:
+        s_log=LogUserEvents.objects.filter(is_visitor=False, logger_id=user[i].id,log_type=11).filter(created__gte=rate_7th_date,created__lt=rate_end_date)
+        if len(s_log)>0:
+            grade_week7[i]=1
+
+
+#number of submit ideas in the 7th week
+submit_week7=np.zeros(len(user))
+for i in range(len(user)):
+    comments=DiscussionComment.objects.filter(user=user[i],created__gte=rate_7th_date,created__lt=rate_end_date)
+    submit_week7[i]=len(comments)
+
+#number of rated ideas in the 7th week
+rate_week7=np.zeros(len(user))
+for i in range(len(user)):
+    ratings=CommentAgreement.objects.filter(rater=user[i],created__gte=rate_7th_date,created__lt=rate_end_date)
+    rate_week7[i]=len(ratings)
+
+
 
 
 #join week
@@ -407,4 +475,4 @@ for i in range(len(user)):
     ratings=CommentAgreement.objects.filter(rater=user[i],created__lte=rate_2nd_date)
     rate_week1[i]=len(ratings)
 
-scipy.io.savemat('mcafe_data_2.mat',dict(baseline_issues_4th=baseline_issues_4th,appear_week4=appear_week4,grade_week4=grade_week4,rate_week4=rate_week4,submit_week4=submit_week4,baseline_issues_3rd=baseline_issues_3rd,appear_week3=appear_week3,grade_week3=grade_week3,submit_week3=submit_week3,rate_week3=rate_week3,appear_week2=appear_week2,rate_week2=rate_week2,submit_week2=submit_week2,grade_week2=grade_week2,baseline_issues_2nd=baseline_issues_2nd,baseline_issues_1st=baseline_issues_1st,join_week=join_week,submit_week1=submit_week1,rate_week1=rate_week1,baseline_issues_5th=baseline_issues_5th,appear_week5=appear_week5,grade_week5=grade_week5,rate_week5=rate_week5,submit_week5=submit_week5))
+scipy.io.savemat('mcafe_data_2.mat',dict(baseline_issues_4th=baseline_issues_4th,appear_week4=appear_week4,grade_week4=grade_week4,rate_week4=rate_week4,submit_week4=submit_week4,baseline_issues_3rd=baseline_issues_3rd,appear_week3=appear_week3,grade_week3=grade_week3,submit_week3=submit_week3,rate_week3=rate_week3,appear_week2=appear_week2,rate_week2=rate_week2,submit_week2=submit_week2,grade_week2=grade_week2,baseline_issues_2nd=baseline_issues_2nd,baseline_issues_1st=baseline_issues_1st,join_week=join_week,submit_week1=submit_week1,rate_week1=rate_week1,baseline_issues_5th=baseline_issues_5th,appear_week5=appear_week5,grade_week5=grade_week5,rate_week5=rate_week5,submit_week5=submit_week5,appear_week6=appear_week6,grade_week6=grade_week6,rate_week6=rate_week6,submit_week6=submit_week6,appear_week7=appear_week7,grade_week7=grade_week7,rate_week7=rate_week7,submit_week7=submit_week7))
